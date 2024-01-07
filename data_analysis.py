f990f4d42e47523067d50b78a39ce7de564fb5dd
Loumr <78451594+Loumr@users.noreply.github.com>
2024-01-07 22:22:15 +0100

    update plotTSP

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 def compare_solutions():
     pass
@@ -26,27 +25,6 @@ def parse_instance(file_path):
 
     return instance
 
-def parse_instance_numpy_array(file_path):
-    file = open(file_path, 'r')
-    Lines = file.readlines()
-    file.close()
-
-    instance = []
-    
-    reading_points = False
-    for line in Lines:
-        if "NODE_COORD_SECTION" in line:
-            reading_points = True
-        elif reading_points:
-            if "EOF" in line:
-                break
-            parts = line.split()
-            if len(parts) >= 3:
-                coordinates = (float(parts[1]), float(parts[2]))
-                instance.append(coordinates)
-
-    return len(instance), np.array(instance), None
-
 def parse_solution(file_path, points):
     file = open(file_path, 'r')
     Lines = file.readlines()
@@ -71,7 +49,7 @@ def parse_solution(file_path, points):
 
 # Source:
 # https://gist.github.com/payoung/6087046
-def plotTSP(paths, points, show=True, save=False, plot_name="Solution Métro Circulaire", file_name="metro_circ"):
+def plotTSP(paths, points, show=True, save=False, plot_name="Solution Métro Circulaire", file_name="metro_circ", mode="points", clusters=None):
     """
     paths: List of lists with the different orders in which the nodes are visited
     points: coordinates for the different nodes
@@ -81,28 +59,50 @@ def plotTSP(paths, points, show=True, save=False, plot_name="Solution Métro Cir
     x = []
     y = []
 
-    for x_p, y_p in points:
-        x.append(x_p)
-        y.append(y_p) 
-    plt.plot(x, y, 'co')
-
+    if clusters is None:
+        for x_p, y_p in points:
+            x.append(x_p)
+            y.append(y_p) 
+        plt.plot(x, y, 'co')
+    
     k = len(paths)-1
     col = 0
     while k>= 0:
         path = paths[k]
-        startx, starty = path[len(path)-1]
-        endx = (0, 0)
-        endy = (0, 0)
-        for i in range(0,len(path)):
-            endx, endy = path[i]
-            plt.arrow(startx, starty, endx - startx, endy - starty,
-                    head_width = 0, color = colors[col%len(colors)], length_includes_head = True)
-            startx = endx
-            starty = endy
-        #plt.arrow(startx, starty, endx - startx, endy - starty,
-        #        head_width = 0, color = colors[col%len(colors)], length_includes_head = True)
+        if mode == "points":
+            startx, starty = path[len(path)-1]
+            endx = (0, 0)
+            endy = (0, 0)
+            for i in range(0,len(path)):
+                endx, endy = path[i]
+                plt.arrow(startx, starty, endx - startx, endy - starty,
+                        head_width = 0, color = colors[col%len(colors)], length_includes_head = True)
+                startx = endx
+                starty = endy
+            #plt.arrow(startx, starty, endx - startx, endy - starty,
+            #        head_width = 0, color = colors[col%len(colors)], length_includes_head = True)
+        elif mode == 'edges':
+            for edge in path:
+                startx, starty = edge[0][0], edge[0][1]
+                endx, endy = edge[1][0], edge[1][1]
+                plt.arrow(startx, starty, endx - startx, endy - starty,
+                        head_width = 0, color = colors[col%len(colors)], length_includes_head = True)
         k -= 1
         col += 1
+
+    if clusters is not None:
+        num_labels = len(clusters)
+        colormap = plt.cm.get_cmap('tab10', num_labels)
+        print("clusters is not none")
+        for l, cluster in clusters.items():
+            xl, yl = [], []
+            for pt in cluster:
+                xl.append(points[pt][0])
+                yl.append(points[pt][1])
+                x.append(points[pt][0])
+                y.append(points[pt][1])
+            print("colormap", colormap(l))
+            plt.scatter(xl, yl, marker='o',label=f'Cluster {l}', color=colormap(l))
 
     s_x = 1
     s_y = 1
@@ -121,7 +121,7 @@ def plotTSP(paths, points, show=True, save=False, plot_name="Solution Métro Cir
 # EXAMPLE:
 
 if __name__ == '__main__':
-    instance = parse_instance("instances/ts225.tsp")
+    instance = parse_instance("instances/tsp225.tsp")
     solution = parse_solution("instances/tsp225.opt.tour", instance)
     print("Instance:")
     print(instance)
